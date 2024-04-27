5fb82efb0aa4ce5c70034f31283449b3ebb5c438
Augustine Jibunoh <jibunoh.augustine@gmail.com>
2024-04-27 11:38:52 +0100

    UPDATE[MODIFY CONFIG]

--- nutriblend/helpers/reusable.py
+++ nutriblend/helpers/reusable.py
@@ -1,9 +1,7 @@
 import base64
-import os
 from requests import exceptions, request
-from string import Template, punctuation
+from string import punctuation
 
-from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as text
 
@@ -38,13 +36,16 @@ def validate_password(password: str):
         )
     if not any(char.isdigit() for char in password):
         raise ValidationError(
-            text("Error: password should have at least one numeric digit."))
+            text("Error: password should have at least one numeric digit.")
+        )
     if not any(char.isupper() for char in password):
         raise ValidationError(
-            text("Error: password should have at least one uppercase character."))
+            text("Error: password should have at least one uppercase character.")
+        )
     if not any(char.islower() for char in password):
         raise ValidationError(
-            text("Error: password should have at least one lowercase character."))
+            text("Error: password should have at least one lowercase character.")
+        )
     if not any(char in special_characters for char in password):
         raise ValidationError(
             text("Error: password should have at least one special character.")
@@ -78,11 +79,7 @@ def make_request(request_type: str, params: dict) -> dict:
     """
     try:
         response = request(request_type, **params)
-        return {
-            "status": True,
-            "data": response.json(),
-            "error": None
-        }
+        return {"status": True, "data": response.json(), "error": None}
     except exceptions.RequestException as error:
         return {
             "status": False,
@@ -102,98 +99,3 @@ def convert_string_to_base32(text: str):
     encoded_bytes = base64.b32encode(text.encode())
     encoded_string = encoded_bytes.decode()
     return encoded_string
-
-
-def email_sender(
-    recipient: list,
-    subject: str,
-    template_directory: str = None,
-    file=None,
-    file_name=None,
-    use_template: bool = False,
-    has_attachment: bool = False,
-    text: str = None,
-    **substitutes
-):
-    """
-    Send an email to one or more recipients using Mailgun API.
-    Args:
-    recipient (list): A list of email addresses of the recipients.
-    subject (str): The subject of the email.
-    template_directory (str, optional): The directory containing the email HTML template file. Defaults to None.
-    file (object, optional): The file to be attached to the email. Defaults to None.
-    file_name (str, optional): The name of the attached file. Defaults to None.
-    use_template (bool, optional): Flag indicating whether to use an HTML template for the email body. Defaults to True.
-    has_attachment (bool, optional): Flag indicating whether the email has an attachment. Defaults to False.
-    text (str, optional): The plain-text body of the email. Used when `use_template` is False. Defaults to None.
-    **substitutes: Keyword arguments used for substituting variables in the email template if `use_template` is True.
-    Returns:
-    dict: A dictionary containing the Mailgun API response. The response can be retrieved using the 'email_sender_response' key.
-    """
-
-    url = settings.MAILGUN_URL
-    api_key = settings.MAILGUN_API_KEY
-
-    if use_template and has_attachment:
-        TEMPLATE_DIR = os.path.join("templates", template_directory)
-        html_temp = os.path.abspath(TEMPLATE_DIR)
-
-        with open(html_temp) as temp_file:
-            template = temp_file.read()
-
-        template = Template(template).safe_substitute(substitutes)
-
-        response = make_request(
-            "POST",
-            dict(
-                url=url,
-                auth=("api", api_key),
-                data={
-                    "from": "Liberty Technologies Ltd. <postmaster@mg.whispersms.com>",
-                    "to": recipient,
-                    "subject": subject,
-                    "html": template,
-                },
-                files=[("attachment", (file_name, open(str(file), "rb").read()))]
-            )
-        )
-    elif use_template:
-        TEMPLATE_DIR = os.path.join("templates", template_directory)
-        html_temp = os.path.abspath(TEMPLATE_DIR)
-
-        with open(html_temp) as temp_file:
-            template = temp_file.read()
-
-        template = Template(template).safe_substitute(substitutes)
-
-        response = make_request(
-            "POST",
-            dict(
-                url=url,
-                auth=("api", api_key),
-                data={
-                    "from": "Liberty Technologies Ltd. <postmaster@mg.whispersms.com>",
-                    "to": recipient,
-                    "subject": subject,
-                    "html": template,
-                }
-            )
-        )
-    else:
-        response = make_request(
-            "POST",
-            dict(
-                url=url,
-                auth=("api", api_key),
-                data={
-                    "from": "Liberty Technologies Ltd. <postmaster@whispersms.com>",
-                    "to": recipient,
-                    "subject": subject,
-                    "text": text
-                }
-            )
-        )
-    if response.get("status") == True:
-        return dict(email_sender_response=response)
-    else:
-        return dict(email_sender_response=response)
