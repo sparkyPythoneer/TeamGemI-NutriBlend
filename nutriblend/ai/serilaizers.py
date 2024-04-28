e00293acb8cc2a07b095e18b7260313f90be16af
chukaibejih <chukaibejih@gmail.com>
2024-04-28 11:29:21 +0100

    new app

---
+++ nutriblend/ai/serilaizers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Conversation, Message
+
+
+class ConversationSerilaizer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Conversation
+        fieldds = ['id', 'user', 'created_at', 'updated_at']
+
+class MessageSerilaizer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ['id', 'conversation', 'sender', 'content', 'created_at', 'updated_at']
--- nutriblend/main/models.py
+++ nutriblend/main/models.py
@@ -1,7 +1,5 @@
 from django.conf import settings
 from django.db import models
-from datetime import datetime
-import uuid
 from core.models import BaseModel
 
 
@@ -50,27 +48,6 @@ class RecipieDetails(BaseModel):
 #     like = models.BooleanField()
 
 
-
-
-
-class Conversation(BaseModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):  
-        return f"Conversation between {self.user.username} and AI"
-
-
-class Message(BaseModel):
-    SENDER = (
-        ('USER', 'User'),
-        ('GEMINI', 'Gemini')
-    )
-    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
-    sender = models.CharField(choices=SENDER, max_length=100, blank=True, null=True)
-    content = models.TextField()
-
-    def __str__(self):
-        return f"{self.sender}: {self.content}"
 
 
 
